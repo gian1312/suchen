1fac9337e36629fb218e8670fee99f301c79b4ff
gian1312 <gian1312@users.noreply.github.com>
2018-03-27 10:12:16 +0200

    Tolles Bash

--- ENV/tensorforce-master/mazerunner/mazebash.bat
+++ ENV/tensorforce-master/mazerunner/mazebash.bat
@@ -1,1 +1,1 @@
-pyton python mazerunner/maze_explorer.py -a examples/configs/ppo.json -n examples/configs/mlp2_network.json --mode 0 --hide
+ python mazerunner/maze_explorer.py -a examples/configs/ppo.json -n examples/configs/mlp2_network.json --mode 0 --hide
